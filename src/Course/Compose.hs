e6da779eca2ba1a0f66e5f101ce165858559107f
Grey Hill <infogreytech@gmail.com>
2017-12-28 17:43:25 -0800

    Finish Compose exercises

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Course.Compose where
 
@@ -10,26 +11,21 @@ import Course.Monad
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
-newtype Compose f g a =
-  Compose (f (g a))
+newtype Compose f g a = Compose (f (g a))
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  h <$> Compose x = Compose $ ((<$>) h) <$> x
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  pure = Compose . pure . pure
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
+-- Implement the (<*>) function for an Applicative instance for Compose
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  Compose f <*> Compose x = Compose $ (pure (<*>) <*> f) <*> x
+
+instance (Monad f, Monad g) => Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+  (=<<) :: (a -> Compose f g b) -> Compose f g a -> Compose f g b
+  (=<<) = undefined
