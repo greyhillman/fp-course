c415aac67bce4ae21b702bec6502a5817a716325
Grey Hill <infogreytech@gmail.com>
2018-01-02 13:12:59 -0800

    Finish Anagram exercises
    words.txt is just a copy of /usr/share/dict/words
    Thanks Robert from Stackoverflow!

--- src/Course/Anagrams.hs
+++ src/Course/Anagrams.hs
@@ -28,17 +28,14 @@ Functions that might help
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-anagrams ::
-  Chars
-  -> FilePath
-  -> IO (List Chars)
-anagrams =
-  error "todo: Course.Anagrams#anagrams"
+anagrams :: Chars -> FilePath -> IO (List Chars)
+anagrams xs filename = help <$> (readFile filename)
+    where
+        help contents = filter (\x -> x `elem` (lines contents)) $ permutations xs
 
 -- Compare two strings for equality, ignoring case
-equalIgnoringCase ::
-  Chars
-  -> Chars
-  -> Bool
-equalIgnoringCase =
-  error "todo: Course.Anagrams#equalIgnoringCase"
+equalIgnoringCase :: Chars -> Chars -> Bool
+equalIgnoringCase Nil Nil = True
+equalIgnoringCase _   Nil = False
+equalIgnoringCase Nil _   = False
+equalIgnoringCase (l:.ls) (r:.rs) = toLower l == toLower r && equalIgnoringCase ls rs
